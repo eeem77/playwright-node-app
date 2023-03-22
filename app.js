9a73c2dff06233228820460a4961ee0c0bf35156
eeem77 <escobarmernestoe@gmail.com>
2023-03-22 16:33:46 -0400

    update function original

--- app.js
+++ app.js
@@ -12,8 +12,9 @@ const login = async (page) => {
   console.log('login: OK')
 }
 
-const extractIds = async (page, url) => {
-  await page.goto(url)
+const extractIds = async (page) => {
+  //await page.goto(url)
+  
   const html = await page.$$eval('.card-aligned', node => node.map(n => n.innerHTML))
   const body = html[0].split('">')
   const filter = body.filter(data => data.includes('product-box'))
@@ -24,8 +25,18 @@ const extractIds = async (page, url) => {
 const web = async () => {
   const browser = await chromium.launch()
   const page = await browser.newPage()
-  const ids = await extractIds(page, 'https://www.apprinting.com/simple-flat-5x7-wedding-invitations/products/')
-  console.log(ids, process.env)
+  // const ids = await extractIds(page, 'https://www.apprinting.com/simple-flat-5x7-wedding-invitations/products/')
+  // console.log(ids)
+  await page.goto('https://www.apprinting.com/simple-flat-5x7-wedding-invitations/products/')
+  //const buttonNext = await page.$('.page-link').getByText('Next  »')
+  const page1 = await extractIds(page)
+  await page.getByText('Next  »').click()
+  await page.waitForLoadState('networkidle')
+  const page2 = await extractIds(page) 
+  const ids = page1.concat(page2)
+  console.log(ids)
+  // await page.locator('tag=a').getByText().click()
+  //await page.screenshot({path: 'uno.png'})
   await browser.close()
 }
 
