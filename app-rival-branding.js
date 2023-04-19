8024b1bcb5e8d04e0235898e4b9c9f63510d912a
eeem77 <escobarmernestoe@gmail.com>
2023-04-18 20:15:24 -0400

    update changes

---
+++ app-rival-branding.js
@@ -0,0 +1,102 @@
+import { chromium, firefox, webkit } from 'playwright'
+import fs from 'fs'
+
+const formBase = async (page, form) => {
+    const buttons = await form.$$('button.btn.dropdown-toggle.btn-dropdown')
+    let btn = 0
+    for (let i = 0; i < buttons.length; i++){
+        i === 0 ? btn = 1 : btn = 0
+        const btns = await form.$$('button.btn.btn-dropdown')
+        await btns[i].click()
+        const menu = await form.$('div.dropdown-menu.show')
+        const menuBtn = await menu.$$('a.dropdown-item')
+        await menuBtn[btn].click()
+        console.log(await btns[i].innerText())
+        await page.waitForTimeout(6000)
+    }
+}
+
+const collapseTrue = async (form) => {
+    const collapse = await form.$$('a.group-collapse-button')
+    await collapse[0].click()
+    await collapse[1].click()
+    await collapse[2].click()
+}
+
+const writeList = async (form) => {
+    const buttonsLabel = await form.$$eval('button.btn.btn-dropdown', node => node.map(n => n.innerText))
+    const price = await form.$eval('#disp_product_price', node => node.innerText)
+    const priceSubtotal = await price.replace(',', '')
+    const priceTotal = await priceSubtotal.replace('$', '')
+    await buttonsLabel.push(priceTotal)
+    fs.appendFileSync(`list.txt`, buttonsLabel.toString() + '\n')
+}
+
+const openList = async (form, button) => {
+    const buttons = await form.$$('button.btn.dropdown-toggle.btn-dropdown')
+    await buttons[button].click()
+    const menu = await form.$('div.dropdown-menu.show')
+    const menuBtn = await menu.$$('a.dropdown-item') 
+    return menuBtn
+}
+
+const changeOptions = async (page, form, button) => {
+    const menuBtn = await openList(form, button)
+    for (let i = 0; i < menuBtn.length; i++){
+        if (i === 0) {
+            await menuBtn[i].click()
+            await page.waitForTimeout(6000)
+            await writeList(form)
+        } else {
+            const menuBtnElse = await openList(form, button)
+            await menuBtnElse[i].click()
+            await page.waitForTimeout(6000)
+            await writeList(form)
+        }
+    }
+}
+
+const changeTwoOptions = async (page, form, button, option, buttonTwo, optionTwo) => {
+    for (let i = 0; i < 2; i++){
+        if (i === 0) {
+            const menuBtn = await openList(form, button)
+            await menuBtn[option].click()
+            await page.waitForTimeout(6000)
+        } else {
+            const menuBtnElse = await openList(form, buttonTwo)
+            await menuBtnElse[optionTwo].click()
+            await page.waitForTimeout(6000)
+        }
+    }
+    const buttonsLabel = await form.$$eval('button.btn.btn-dropdown', node => node.map(n => n.innerText))
+    console.log(buttonsLabel)
+}
+
+const web = async () => {
+    const browser = await chromium.launch()
+    const page = await browser.newPage()
+    await page.goto('https://www.printingcenterusa.com/printing/booklet-printing')
+    const form = await page.$('form')
+    await collapseTrue(form)
+    await formBase(page, form)
+
+    // for (let i = 0; i <= 20; i++){
+    //     await changeTwoOptions(page, form, 0, 12, 5, i)
+    //     await changeOptions(page, form, 1)
+    //     fs.appendFileSync(`list.txt`, '\n\n\n')
+    // }
+
+    for (let i = 8; i <= 12; i++){
+        const ciclo = i
+        for (let i = 0; i <= 3; i++){
+            await changeTwoOptions(page, form, 0, ciclo, 10, i)
+            await changeOptions(page, form, 1)
+            fs.appendFileSync(`list.txt`, '\n\n\n')
+        }
+    }
+
+    console.log('END')
+    await browser.close()
+}
+
+web()
---
+++ update-price.js
@@ -0,0 +1,50 @@
+import { chromium, firefox, webkit } from 'playwright'
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+import listPrice from './listPrice.js'
+
+const url = 'https://www.apprinting.com/admin/'
+const urlProductUpdatePrice = 'https://www.apprinting.com/admin/product_additionalinfo_price.php?product_id=3201&prod_add_opt_id=86046&sel_product_size=3456'
+
+const qtys = [10,25,50,100,250,500,750,1000,1500,2000,2500,3000,4000,5000,6000,7000]
+
+const login = async (page) => {
+    await page.goto(url)
+    const user = await page.$('#username')
+    const pass = await page.$('#password')
+    const btn = await page.$('button')
+    await user.fill(process.env.LOGIN_USER)
+    await pass.fill(process.env.LOGIN_SECRET_KEY)
+    await btn.click()
+    await page.waitForTimeout(3000)
+    console.log('login: OK')
+}
+//txtprice[10_1829044_]  txtprice[10_1829064_]
+const inputFillToPrice = async (page) => {
+    await page.goto(urlProductUpdatePrice)
+    let price = 0
+    for (let i = 44; i <= 64; i++){
+        for await (const qty of qtys){
+            const id = `txtprice[${qty}_18290${i}_]`
+            const inputPrice = await page.$(`[id="${id}" ]`)
+            await inputPrice.fill(listPrice[price].toString())
+            console.log(listPrice[price].toString());
+            price++
+        }
+    }
+    const btnSave = await page.$('#btn-action-save')
+    await btnSave.click()
+    await page.waitForTimeout(5000)
+}
+
+const updatePrice = async () => {
+    const browser = await chromium.launch()
+    const page = await browser.newPage()
+    await login(page)
+    await inputFillToPrice(page)
+    console.log('END')
+    await browser.close()
+}
+
+updatePrice()
