1cc2eec5ba79e668b61a22f7e910e54e44ee2796
eeem77 <escobarmernestoe@gmail.com>
2023-05-16 20:21:50 -0400

    update

--- update-price.js
+++ update-price.js
@@ -5,7 +5,7 @@ dotenv.config()
 import listPrice from './listPrice.js'
 
 const url = 'https://www.apprinting.com/admin/'
-const urlProductUpdatePrice = 'https://www.apprinting.com/admin/product_additionalinfo_price.php?product_id=3666&prod_add_opt_id=95762&sel_product_size=4126'
+const urlProductUpdatePrice = 'https://www.apprinting.com/admin/product_additionalinfo_price.php?product_id=3666&prod_add_opt_id=95764&sel_product_size=4126'
 
 const qtys = [
     // 25,
@@ -49,26 +49,26 @@ const qtys = [
 ]
 
 const login = async (page) => {
-    await page.goto(url, { timeout: 90000 })
+    await page.goto(url, { timeout: 30000 })
     const user = await page.$('#username')
     const pass = await page.$('#password')
     const btn = await page.$('button')
     await user.fill(process.env.LOGIN_USER)
     await pass.fill(process.env.LOGIN_SECRET_KEY)
     await btn.click()
-    await page.waitForTimeout(7000)
+    await page.waitForTimeout(3000)
     console.log('login: OK')
 }
 // 197txtprice[25_1971197_] txtprice[100000_1971197_] Setup Attribute Price For - Half Fold
 // 198 Setup Attribute Price For - Trifold/Letter Fold 
 // 199 Setup Attribute Price For - Z-Fold
 // 205 Setup Attribute Price For - Roll Fold (4 panels) 
-// txtprice[25_1971208_]
+// txtprice[25_1971212_]
 const inputFillToPrice = async (page) => {
     await page.goto(urlProductUpdatePrice)
     let price = 0
     //let postNumber = 9386866  ${postNumber}
-    for (let i = 210; i <= 210; i++){
+    for (let i = 215; i <= 216; i++){
         for await (const qty of qtys){
             const id = `txtprice[${qty}_1971${i}_]`
             const inputPrice = await page.$(`[id="${id}" ]`)
