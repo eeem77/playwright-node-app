9540273e2e313cf066b6a0e669b1b57b5abc6e0e
eeem77 <escobarmernestoe@gmail.com>
2023-05-18 10:13:11 -0400

    update

--- flyers.js
+++ flyers.js
@@ -155,7 +155,7 @@ const searchLen = async (page, form, option) => {
 const web = async () => {
     const browser = await chromium.launch()
     const page = await browser.newPage()
-    await page.goto('https://www.uprinting.com/business-flyers-printing.html?aind=prod_up_products&aqid=7bf96f9d338305ea57e1b805dd59a128&aoid=f331c17e6a882f29f01fad7e1f17782493060fd80603ce009d25930af8722460&apos=1&aut=87a96bd2-f4e3-11ed-be23-0242ac110002-1684349358&asrc=results_page&akywd=Flyers&stype=algolia&mdl=products', {timeout:90000})
+    await page.goto('https://www.uprinting.com/business-flyers-printing.html?aind=prod_up_products&aqid=7bf96f9d338305ea57e1b805dd59a128&aoid=f331c17e6a882f29f01fad7e1f17782493060fd80603ce009d25930af8722460&apos=1&aut=87a96bd2-f4e3-11ed-be23-0242ac110002-1684349358&asrc=results_page&akywd=Flyers&stype=algolia&mdl=products', {timeout:150000})
     const form = await page.$('#product_calculator_form')
     
     
@@ -168,10 +168,11 @@ const web = async () => {
     //         fs.appendFileSync(`list.txt`, '\n\n\n')
     //     }
     // }
-
-    await changeTwoOptions(page, form, 0, 11, 2, 10)
-    await changeOptions(page, form, 5)
-    fs.appendFileSync(`list.txt`, '\n\n\n')
+    for (let i = 7; i <= 11; i++){
+        await changeTwoOptions(page, form, 0, i, 3, 1)
+        await changeOptions(page, form, 5)
+        fs.appendFileSync(`list.txt`, '\n\n\n')
+    }
     
  
     console.log('END')
