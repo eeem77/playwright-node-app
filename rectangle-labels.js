a130c5d482c4798f65cdd20e1d361f5cff97aab4
eeem77 <escobarmernestoe@gmail.com>
2023-05-31 15:20:29 -0400

    update

--- rectangle-labels.js
+++ rectangle-labels.js
@@ -204,9 +204,9 @@ const web = async () => {
 
     const options = await searchLen(form, 1)
     
-    await changeOneOptions(form, 0, 9) //size
+    await changeOneOptions(form, 0, 10) //size
     //console.log(options)
-    for (let i = 11; i < options; i++) {
+    for (let i = 1; i < options; i++) {
         await changeOneOptions(form, 1, i)
         for await (const quantity of quantitys) {
             const qty = await form.$('#prdqty')
