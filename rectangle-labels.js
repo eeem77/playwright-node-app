8395a8c5058d092ad1160b7d829bf4a57fef0e9b
eeem77 <escobarmernestoe@gmail.com>
2023-05-31 20:47:01 -0400

    update

--- rectangle-labels.js
+++ rectangle-labels.js
@@ -190,7 +190,7 @@ const quantitys = [
 const web = async () => {
     const browser = await chromium.launch({
         proxy: {
-          server: '206.189.50.120:8080'
+          server: '5.75.233.147:8080'
           //username: 'usr',
           //password: 'pwd'
         }
@@ -204,9 +204,9 @@ const web = async () => {
 
     const options = await searchLen(form, 1)
     
-    await changeOneOptions(form, 0, 11) //size
+    await changeOneOptions(form, 0, 12) //size
     //console.log(options)
-    for (let i = 8; i < options; i++) {
+    for (let i = 1; i < options; i++) {
         await changeOneOptions(form, 1, i)
         for await (const quantity of quantitys) {
             const qty = await form.$('#prdqty')
