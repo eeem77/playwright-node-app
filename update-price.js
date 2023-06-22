ccd0ca1d62621d604272307fe981f2fc2dafcfa9
eeem77 <escobarmernestoe@gmail.com>
2023-06-22 11:07:34 -0400

    update

--- update-price.js
+++ update-price.js
@@ -5,7 +5,7 @@ dotenv.config()
 import listPrice from './listPrice.js'
 
 const url = 'https://www.apprinting.com/admin/'
-const urlProductUpdatePrice = 'https://www.apprinting.com/admin/product_additionalinfo_price.php?product_id=3686&prod_add_opt_id=96159&sel_product_size=4204'
+const urlProductUpdatePrice = 'https://www.apprinting.com/admin/product_additionalinfo_price.php?product_id=3686&prod_add_opt_id=96159&sel_product_size=4227'
 
 const qtys = [
 100,
