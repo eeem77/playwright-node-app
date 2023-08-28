3308c7b78fa4fca1b435df876f8cb0ea78397802
eeem77 <escobarmernestoe@gmail.com>
2023-08-28 09:05:27 -0400

    update

--- update-price.js
+++ update-price.js
@@ -5,22 +5,28 @@ dotenv.config()
 import listPrice from './listPrice.js'
 
 const url = 'https://www.apprinting.com/admin/'
-const urlProductUpdatePrice = 'https://www.apprinting.com/admin/product_additionalinfo_price.php?product_id=3147&prod_add_opt_id=96563&sel_product_size=4427'
+const urlProductUpdatePrice = 'https://www.apprinting.com/admin/product_additionalinfo_price.php?product_id=1639&prod_add_opt_id=53841&sel_product_size=4472'
 
 const qtys = [
-    35,
-    40,
-    50,
-    60,
-    75,
-    100,
-    150,
-    200,
-    250,
-    500,
     1000,
     2000,
-    5000
+    2500,
+    3000,
+    4000,
+    5000,
+    6000,
+    7000,
+    8000,
+    9000,
+    10000,
+    15000,
+    20000,
+    25000,
+    30000,
+    35000,
+    40000,
+    45000,
+    50000
 ]
 
 const login = async (page) => {
@@ -39,17 +45,17 @@ const inputFillToPrice = async (page) => {
     await page.goto(urlProductUpdatePrice, { timeout: 300000 })
     let price = 0
     //let postNumber = 9607838  ${postNumber}
-    for (let i = 259; i <= 262; i++) {
+    for (let i = 258; i <= 259; i++) {
         //if( i !== 244){
-            for await (const qty of qtys) {
-                const id = `txtprice[${qty}_1980${i}_]`
-                const inputPrice = await page.$(`[id="${id}" ]`)
-                await inputPrice.fill(listPrice[price].toString())
-                console.log(listPrice[price].toString());
-                price++
-                //console.log(postNumber);
-                //postNumber++ 
-            }
+        for await (const qty of qtys) {
+            const id = `txtprice[${qty}_1352${i}_]`
+            const inputPrice = await page.$(`[id="${id}" ]`)
+            await inputPrice.fill(listPrice[price].toString())
+            console.log(listPrice[price].toString());
+            price++
+            //console.log(postNumber);
+            //postNumber++ 
+        }
         //}
     }
     const btnSave = await page.$('#btn-action-save')
