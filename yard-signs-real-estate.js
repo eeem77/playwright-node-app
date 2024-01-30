5637208d0ef90916958efb90596e8adfe0759ad5
eeem77 <escobarmernestoe@gmail.com>
2024-01-29 21:46:01 -0400

    update

--- yard-signs-real-estate.js
+++ yard-signs-real-estate.js
@@ -3,7 +3,7 @@ import fs from 'fs'
 import path from 'path'
 
 const writeList = async (page) => {
-    const buttonsLabel = await page.$$eval('button.btn.dropdown-toggle.val-wrap', node => node.map(n => n.innerText))
+    const buttonsLabel = await page.$$eval('.dropdown-toggle-label', node => node.map(n => n.innerText))
     const price = await page.$eval('.calc-price.subtotal-price', node => node.innerText)
     const priceSubtotal = await price.replace(',', '')
     const priceTotal = await priceSubtotal.replace('$', '')
@@ -12,17 +12,17 @@ const writeList = async (page) => {
 }
 
 const openList = async (page, button) => {
-    console.log('entro en la funcion openList');
-    await page.waitForSelector('button.btn.dropdown-toggle',{state: 'attached'})
-    const buttons = await page.$$('button.btn.dropdown-toggle')
+    //console.log('entro en la funcion openList');
+    //await page.waitForSelector('button.btn.dropdown-toggle',{state: 'attached'})
+    const buttons = await page.$$('[name="attr5"]')
     await buttons[button].click()
-    console.log('Working');
     //await page.waitForTimeout(3000)
     //await page.screenshot({path:'./eeem77.jpg'})
     const menu = await page.$('div.site-dropdown.dropdown.expanded')
     const menuBtn = await menu.$$('a.attr-value.val-wrap ')
-    const menuBtnText = await menu.$$eval('a.attr-value.val-wrap ', node => node.map(n => n.innerText))
+    const menuBtnText = await menu.$$eval('.dropdown-toggle-label', node => node.map(n => n.innerText))
 
+    console.log('Working');
     console.log(menuBtnText)
     return menuBtn
 }
@@ -47,7 +47,7 @@ const changeOptions = async (page, button) => {
                 //console.log(menuBtn.length);
                 await menuBtn[i].click()
                 //await page.waitForSelector('#price')
-                await page.waitForTimeout(5000)
+                await page.waitForTimeout(10000)
                 //await page.screenshot({ path: './pepa.jpg' })
                 //await page.waitForSelector('#price',{state: 'attached'})
                 await writeList(page)
@@ -56,7 +56,7 @@ const changeOptions = async (page, button) => {
                 const menuBtnDefault = await openList(page, button)
                 await menuBtnDefault[i].click()
                 //await page.waitForSelector('#price')
-                await page.waitForTimeout(5000)
+                await page.waitForTimeout(10000)
                 //await page.waitForSelector('#price',{state: 'attached'})
                 await writeList(page)
                 break
@@ -81,10 +81,10 @@ const web = async () => {
     //       //password: 'pwd'
     //     }
     // })
-    const browser = await firefox.launch()
+    const browser = await chromium.launch()
     const page = await browser.newPage()
-    await page.goto('https://www.uprinting.com/real-estate-a-frame-signs.html', {timeout:200000})
-    
+    await page.goto('https://www.uprinting.com/real-estate-a-frame-signs.html', { timeout: 200000 })
+
 
     await page.waitForTimeout(15000)
     // class="multi-calc-button-container ng-scope"
@@ -103,15 +103,27 @@ const web = async () => {
     //   ]
 
     //await btnDisplayOptions.waitForElementState('')
-    await page.waitForSelector('div.multi-calc-button.even',{state: 'visible'})
-    const btnDisplayOptions = await page.$('div.multi-calc-button.even')
-    await btnDisplayOptions.click({force: true})
+
+    await page.waitForSelector('div.multi-calc-button.even', { state: 'visible' })
+    const btnDisplayOptions = await page.$$('.multi-calc-button-container.ng-scope')
+    console.log(btnDisplayOptions.length);
+    await btnDisplayOptions[1].click()
     //await page.getByLabel('div.multi-calc-button.even').fill('true');
-    //await page.waitForTimeout(3000)
-    await page.waitForSelector('#price',{state: 'attached'})
-    await page.screenshot({path:'./eeem77.jpg'})
-    const optionsOne = await page.$$eval('.btn.dropdown-toggle.val-wrap', node => node.map(n => n.innerText))
-    console.log(optionsOne);
+    await page.waitForTimeout(10000)
+    await page.screenshot({ path: './screenshot.png', fullPage: true });
+    //const fondo = await page.$('.calculator-shipping-container')
+    const price = await page.$$('.accessibility-price.hidden')
+    
+    console.log(await price[1].$eval('span.calc-price.subtotal-price', node => node.innerText));
+    //await page.waitForSelector('#price',{state: 'attached'})
+    //    const optionsOne = await page.$$eval('.btn.dropdown-toggle.val-wrap', node => node.map(n => n.innerText))
+    //    console.log(optionsOne);
+
+    //    const prueba = await page.$$('[name="attr5"]')
+    //    await prueba[2].click()
+    //await page.screenshot({ path: './eeem77.jpg' })
+    //[ '1', '', '1', '' ]
+    //console.log(prueba);
     //await changeOneOptions(page, 0, 0)
     //await changeOneOptions(page, 0, 1)
     // await changeOneOptions(page, 1, 4)
@@ -119,8 +131,8 @@ const web = async () => {
     // await changeOneOptions(page, 5, 2)
     // const optionsOne = await page.$$eval('.btn.dropdown-toggle.val-wrap', node => node.map(n => n.innerText))
     // console.log(optionsOne);
-    
-    await changeOptions(page, 9)
+
+    //await changeOptions(page, 2)
 
 
     console.log('END')
