457a27b178c7b8698aed9963f1681dd7b4c48c24
eeem77 <escobarmernestoe@gmail.com>
2024-01-31 19:55:15 -0400

    update

--- update-price.js
+++ update-price.js
@@ -6,15 +6,12 @@ import listPrice from "./listPrice.js";
 
 const url = "https://www.apprinting.com/admin/";
 const urlProductUpdatePrice =
-  "https://www.apprinting.com/admin/product_additionalinfo_price.php?product_id=4409&prod_add_opt_id=100433";
+  "https://www.apprinting.com/admin/product_additionalinfo_price.php?product_id=4410&prod_add_opt_id=100438&sel_product_size=6440";
 
 const qtys = [
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
   23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41,
-  42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
-  61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79,
-  80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98,
-  99, 100,
+  42, 43, 44, 45, 46, 47, 48, 49, 50,
 ];
 
 const login = async (page) => {
@@ -33,7 +30,7 @@ const inputFillToPrice = async (page) => {
   await page.goto(urlProductUpdatePrice, { timeout: 300000 });
   let price = 0;
   //let postNumber = 9607838  ${postNumber}
-  for (let i = 76; i <= 77; i++) {
+  for (let i = 79; i <= 79; i++) {
     //if( i == 39 || i == 41){
     for await (const qty of qtys) {
       const id = `txtprice[${qty}_20056${i}_]`;
