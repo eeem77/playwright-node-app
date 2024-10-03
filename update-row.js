b87023b9be62d32cc238fbf243a25df8806cdaa3
eeem77 <escobarmernestoe@gmail.com>
2024-10-03 13:24:46 -0400

    update

--- update-row.js
+++ update-row.js
@@ -1,49 +1,55 @@
 import { firefox } from 'playwright'
 import {
   login,
-  // auditArtwork,
-  updateAndCreateArtwork
+  auditArtwork
+  // updateAndCreateArtwork
 } from './function_list.js'
-import { proxies } from './data.js'
-import fs from 'fs'
+// import { proxies } from './data.js'
+// import fs from 'fs'
 
 const updateRow = async () => {
-  const proxiesLen = proxies.length
-  const rand = Math.floor(Math.random() * proxiesLen)
-  const ipProxy = proxies[rand]
-  const browser = await firefox.launch({
-    proxy: {
-      server: ipProxy
-      // username: '',
-      // password: ''
-    }
-  })
-  // const browser = await firefox.launch()
+  // const proxiesLen = proxies.length
+  // const rand = Math.floor(Math.random() * proxiesLen)
+  // const ipProxy = proxies[rand]
+  // const browser = await firefox.launch({
+  //   proxy: {
+  //     server: ipProxy
+  //     // username: '',
+  //     // password: ''
+  //   }
+  // })
+  const browser = await firefox.launch()
   const page = await browser.newPage()
 
   // LOGIN APP
   // CREATE AND UPDATE ARTWORK WITH PROXIES LIST
-  if (proxiesLen >= 1) {
-    try {
-      await login(page, ipProxy)
-      await updateAndCreateArtwork(page, ipProxy)
-    } catch (error) {
-      fs.appendFileSync('bad-proxies.txt', `${ipProxy}\n`)
-      console.log(`no connection ---> ${proxiesLen}`)
-      const index = proxies.indexOf(ipProxy)
-      proxies.splice(index, 1)
-      await browser.close()
-      await updateRow()
-    }
-  }
+  // if (proxiesLen >= 1) {
+  //   try {
+  //     await login(page, ipProxy)
+  //     await updateAndCreateArtwork(page, ipProxy)
+  //   } catch (error) {
+  //     fs.appendFileSync('bad-proxies.txt', `${ipProxy}\n`)
+  //     console.log(`no connection ---> ${proxiesLen}`)
+  //     const index = proxies.indexOf(ipProxy)
+  //     proxies.splice(index, 1)
+  //     await browser.close()
+  //     await updateRow()
+  //   }
+  // }
 
   // CREATE AND UPDATE ARTWORK
-  // await login(page)
-  // await updateAndCreateArtwork(page)
+  // try {
+  //   await login(page)
+  //   await updateAndCreateArtwork(page)
+  // } catch (error) {
+  //   console.log('no connection')
+  //   await browser.close()
+  //   await updateRow()
+  // }
 
   // AUDIT ARTWORK OPTIONS
-  // await login(page)
-  // await auditArtwork(page)
+  await login(page)
+  await auditArtwork(page)
 
   // FUNCTIONS GROUPS
   // await getTitleProduct(page);
