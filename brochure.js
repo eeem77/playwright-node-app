1e5e7b63063f5599eaf02cfb05a4fea71ade89d0
eeem77 <escobarmernestoe@gmail.com>
2024-10-21 09:41:13 -0400

    update

--- brochure.js
+++ brochure.js
@@ -1,6 +1,7 @@
 import { chromium, firefox, webkit } from "playwright";
 import fs from "fs";
 import path from "path";
+import player from "play-sound";
 
 const formBase = async (page, form) => {
   let clickList = 2;
@@ -21,7 +22,7 @@ const formBase = async (page, form) => {
     const listActive = await buttonListActive.$$("a.val-wrap.ng-scope");
     await listActive[clickList].click();
     console.log(await buttons[i].innerText());
-    await page.waitForTimeout(5000);
+    await page.waitForTimeout(3000);
   }
   const buttonsInnerText = await form.$$eval(
     "button.btn.dropdown-toggle.val-wrap",
@@ -42,10 +43,7 @@ const writeList = async (form) => {
     "button.btn.dropdown-toggle.val-wrap",
     (node) => node.map((n) => n.innerText)
   );
-  const price = await form.$eval(
-    ".ng-binding.subtotal-price",
-    (node) => node.innerText
-  );
+  const price = await form.$eval("#price", (node) => node.innerText);
   const priceSubtotal = await price.replace(",", "");
   const priceTotal = await priceSubtotal.replace("$", "");
   await buttonsLabel.push(priceTotal);
@@ -57,10 +55,7 @@ const writeListTotal = async (form) => {
     "button.btn.dropdown-toggle.val-wrap",
     (node) => node.map((n) => n.innerText)
   );
-  const price = await form.$eval(
-    ".ng-binding.subtotal-price",
-    (node) => node.innerText
-  );
+  const price = await form.$eval("#price", (node) => node.innerText);
   const priceSubtotal = await price.replace(",", "");
   const priceTotal = await priceSubtotal.replace("$", "");
   await buttonsLabel.push(priceTotal);
@@ -68,54 +63,61 @@ const writeListTotal = async (form) => {
 };
 
 const openList = async (page, form, button) => {
-  await page.waitForSelector("form.calculator");
+  await page.waitForTimeout(3000);
   const buttons = await form.$$("button.btn.dropdown-toggle");
-  //   const buttons = await form.$$eval('button.btn.dropdown-toggle', node => node.map(n => n.innerText))
-  //   console.log(buttons);
-
+  // const buttonsValues = await form.$$eval(
+  //   "button.btn.dropdown-toggle",
+  //   (node) => node.map((n) => n.innerText)
+  // );
+  // console.log(buttonsValues);
+  // await buttons[button].scrollIntoViewIfNeeded();
   await buttons[button].click();
-  await page.waitForSelector(
-    "div.expanded"
-  );
-  const menu = await form.$(
-    "div.expanded"
-  );
-
-  const menuBtn = await menu.$$eval("a", node => node.map(n => n.innerText));
-  console.log(menuBtn);
-  //   console.log("Working");
-  //   return menuBtn;
+  await form.waitForSelector("div.expanded");
+  const menu = await form.$("div.expanded");
+  const menuBtn = await menu.$$("li.dropdown-menu-item");
+  //   const menuBtn = await menu.$$eval("a", (node) =>
+  //     node.map((n) => n.innerText)
+  //   );
+  // console.log(menuBtn);
+  console.log("Working");
+  return menuBtn;
 };
 
 const changeOptions = async (page, form, button) => {
   const menuBtn = await openList(page, form, button);
-//   for (let i = 0; i < menuBtn.length; i++) {
-//     switch (i) {
-//       case 0:
-//         await menuBtn[i].click();
-//         await form.waitForSelector(".ng-binding.subtotal-price", {
-//           state: "attached",
-//         });
-//         //await page.waitForTimeout(5000)
-//         await writeList(form);
-//         break;
-//       default:
-//         const menuBtnDefault = await openList(form, button);
-//         await menuBtnDefault[i].click();
-//         await form.waitForSelector(".ng-binding.subtotal-price", {
-//           state: "attached",
-//         });
-//         //await page.waitForTimeout(5000)
-//         await writeList(form);
-//         break;
-//     }
-//   }
+  const container = await page.$(".page-product-container");
+  for (let i = 6; i < menuBtn.length; i++) {
+    switch (i) {
+      case 6:
+        await menuBtn[i].click();
+        // await form.waitForSelector("#price", {
+        //   state: "attached",
+        // });
+        await container.hover();
+        await page.waitForTimeout(3000);
+        await writeList(form);
+        break;
+      default:
+        const menuBtnDefault = await openList(page, form, button);
+        await menuBtnDefault[i].click();
+        // await form.waitForSelector("#price", {
+        //   state: "attached",
+        // });
+        await container.hover();
+        await page.waitForTimeout(3000);
+        await writeList(form);
+        break;
+    }
+  }
 };
 
 const changeOneOptions = async (page, form, button, option) => {
-  const menuBtn = await openList(form, button);
+  const menuBtn = await openList(page, form, button);
   await menuBtn[option].click();
-  await page.waitForTimeout(5000);
+  //   await form.waitForSelector("#price", {
+  //     state: "attached",
+  //   });
+  await page.waitForTimeout(3000);
 };
 
 const changeTwoOptions = async (
@@ -131,12 +133,12 @@ const changeTwoOptions = async (
       case 0:
         const menuBtn = await openList(form, button);
         await menuBtn[option].click();
-        await page.waitForTimeout(5000);
+        await page.waitForTimeout(3000);
         break;
       case 1:
         const menuBtnElse = await openList(form, buttonTwo);
         await menuBtnElse[optionTwo].click();
-        await page.waitForTimeout(5000);
+        await page.waitForTimeout(3000);
         break;
     }
   }
@@ -163,7 +165,7 @@ const changeThreeOptions = async (
       case 0:
         const menuBtn = await openList(form, button);
         await menuBtn[option].click();
-        await form.waitForSelector(".ng-binding.subtotal-price", {
+        await form.waitForSelector("#price", {
           state: "attached",
         });
         //await page.waitForTimeout(5000)
@@ -171,7 +173,7 @@ const changeThreeOptions = async (
       case 1:
         const menuBtnTwo = await openList(form, buttonTwo);
         await menuBtnTwo[optionTwo].click();
-        await form.waitForSelector(".ng-binding.subtotal-price", {
+        await form.waitForSelector("#price", {
           state: "attached",
         });
         //await page.waitForTimeout(5000)
@@ -179,7 +181,7 @@ const changeThreeOptions = async (
       case 2:
         const menuBtnThree = await openList(form, buttonThree);
         await menuBtnThree[optionThree].click();
-        await form.waitForSelector(".ng-binding.subtotal-price", {
+        await form.waitForSelector("#price", {
           state: "attached",
         });
         //await page.waitForTimeout(5000)
@@ -194,21 +196,38 @@ const bindingElementsOptions = async (page, form, option) => {
     ".custom-control.custom-radio.element_class"
   );
   await bindingElements[option].click();
-  await page.waitForTimeout(5000);
+  await page.waitForTimeout(3000);
 };
 
 const web = async () => {
-  const browser = await chromium.launch();
+  const userDataDir =
+    "C:\\Users\\eeem77\\AppData\\Local\\Google\\Chrome\\User Data\\Default";
+  const browser = await chromium.launchPersistentContext(userDataDir, {
+    headless: false, // Cambia a true si no quieres que se muestre la ventana
+    viewport: { width: 1440, height: 870 },
+  });
+
+  // const context = await browser.newContext({
+  //   viewport: { width: 1920, height: 1080 } // Ajusta el tamaño según tus necesidades
+  // });
+
+  // const page = await context.newPage();
+  // const browser = await chromium.launch();
   const page = await browser.newPage();
   await page.goto("https://www.uprinting.com/brochure-printing.html");
   const form = await page.$("#calc_4_tradpricegroup");
 
   // await changeThreeOptions(page, form, 0, 8, 2, 2, 3, 1)
-  await changeOptions(page, form, 3);
+  await changeOneOptions(page, form, 0, 0);
+  await changeOneOptions(page, form, 5, 0); // 1 none | 3 2-panel | 7-9 3-panel | 17-21 4-panel | 32 6-panel
+  await changeOptions(page, form, 9);
   fs.appendFileSync(`list.txt`, "\n\n\n");
 
   console.log("END");
   await browser.close();
+  player().play("./alarm.mp3", (err) => {
+    if (err) throw err;
+  });
 };
 
 web();
