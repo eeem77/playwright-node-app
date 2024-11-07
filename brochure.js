603d3ffefbfb045604956a42f729a825d7fa9ce4
eeem77 <escobarmernestoe@gmail.com>
2024-11-07 12:18:34 -0400

    update

--- brochure.js
+++ brochure.js
@@ -63,13 +63,12 @@ const writeListTotal = async (form) => {
 };
 
 const openList = async (page, form, button) => {
-  await page.waitForTimeout(3000);
   const buttons = await form.$$("button.btn.dropdown-toggle");
-  const buttonsValues = await form.$$eval(
-    "button.btn.dropdown-toggle",
-    (node) => node.map((n) => n.innerText)
-  );
-  console.log(buttonsValues);
+  // const buttonsValues = await form.$$eval(
+  //   "button.btn.dropdown-toggle",
+  //   (node) => node.map((n) => n.innerText)
+  // );
+  // console.log(buttonsValues);
   // await buttons[button].scrollIntoViewIfNeeded();
   await buttons[button].click();
   await form.waitForSelector("div.expanded");
@@ -83,28 +82,28 @@ const openList = async (page, form, button) => {
   return menuBtn;
 };
 
-const changeOptions = async (page, form, button) => {
+const changeOptions = async (page, form, button, key) => {
   const menuBtn = await openList(page, form, button);
   const container = await page.$(".page-product-container");
-  for (let i = 6; i < menuBtn.length; i++) {
+  for (let i = key; i < menuBtn.length; i++) {
     switch (i) {
-      case 6:
+      case key:
         await menuBtn[i].click();
-        // await form.waitForSelector("#price", {
-        //   state: "attached",
-        // });
         await container.hover();
-        await page.waitForTimeout(3000);
+        await form.waitForSelector("#compute_price_loader", {
+          state: "hidden",
+        });
+        // await page.waitForTimeout(3000);
         await writeList(form);
         break;
       default:
         const menuBtnDefault = await openList(page, form, button);
         await menuBtnDefault[i].click();
-        // await form.waitForSelector("#price", {
-        //   state: "attached",
-        // });
         await container.hover();
-        await page.waitForTimeout(3000);
+        await form.waitForSelector("#compute_price_loader", {
+          state: "hidden",
+        });
+        // await page.waitForTimeout(3000);
         await writeList(form);
         break;
     }
@@ -114,10 +113,10 @@ const changeOptions = async (page, form, button) => {
 const changeOneOptions = async (page, form, button, option) => {
   const menuBtn = await openList(page, form, button);
   await menuBtn[option].click();
-  //   await form.waitForSelector("#price", {
-  //     state: "attached",
-  //   });
-  await page.waitForTimeout(3000);
+  await form.waitForSelector("#compute_price_loader", {
+    state: "hidden",
+  });
+  // await page.waitForTimeout(3000);
 };
 
 const changeTwoOptions = async (
@@ -217,12 +216,83 @@ const web = async () => {
   await page.goto("https://www.uprinting.com/brochure-printing.html");
   const form = await page.$("#calc_4_tradpricegroup");
 
-  // await changeThreeOptions(page, form, 0, 8, 2, 2, 3, 1)
-  await changeOneOptions(page, form, 0, 8);
-  // await changeOneOptions(page, form, 4, 3); // 1 none | 3 2-panel | 7-9 3-panel | 17-21 4-panel | 32 6-panel
-  await changeOneOptions(page, form, 8, 2);
-  await changeOptions(page, form, 9);
-  fs.appendFileSync(`list.txt`, "\n\n\n");
+  for (let index = 0; index <= 8; index++) {
+    await changeOneOptions(page, form, 0, index);
+    await changeOneOptions(page, form, 3, 0);
+    let folding = [];
+    if (index === 0) folding = [1, 3];
+    if (index === 1) folding = [1, 3, 7, 8];
+    if (index === 2) folding = [1, 3, 7, 8, 9, 17, 18, 19, 20, 21];
+    if (index === 3) folding = [1, 3];
+    if (index === 4) folding = [1, 3, 7, 8, 9, 17, 18, 19, 20, 21];
+    if (index === 5) folding = [1, 3, 7, 8, 9, 17, 18, 19, 20, 21];
+    if (index === 6) folding = [1, 3, 7, 8, 9, 17, 18, 19, 20, 21];
+    if (index === 7) folding = [1, 3, 7, 8, 9, 17, 18, 19, 20, 21];
+    if (index === 8) folding = [1, 3, 7, 8, 9, 17, 18, 19, 20, 21];
+    for await (const fol of folding) {
+      await changeOneOptions(page, form, 4, fol);
+      if (fol === 1) {
+        await changeOneOptions(page, form, 5, 0);
+      } else {
+        await changeOneOptions(page, form, 5, 2);
+      }
+      if (index !== 8) {
+        await changeOptions(page, form, 9, 0);
+      } else {
+        await changeOptions(page, form, 9, 6);
+      }
+      fs.appendFileSync(`list.txt`, "\n\n\n");
+    }
+  }
+
+  /*
+  [
+  '8.5" x 11"',
+  '5.5"',
+  '4"',
+  '100 lb. Paper Gloss',
+  'Tri-Fold/Letter Fold',
+  'Outside and Inside',
+  'None',
+  '25',
+  'None',
+  '500',
+  '3 Business Days'
+  ]
+  await changeOneOptions(page, form, 4, 3);
+  ---  2-panel ---
+  3 Half Fold
+  4 Half Fold (with scoring)
+  5 Half Fold (scored only)
+  --- 3-panel ---
+  7 Tri-Fold/Letter Fold
+  8 Z-Fold
+  9 Gate Fold
+  10 Trifold/Letter Fold (with scoring)
+  11 Z-Fold (with scoring)
+  12 Gate Fold (with scoring)
+  13 Trifold/Letter Fold (scored only)
+  14 Z-Fold (scored only)
+  15 Gate Fold (scored only)
+  --- 4-Panel ---
+  17 Accordion Fold (4 panels)
+  18 Double Gate Fold
+  19 Double Parallel Fold
+  20 French Fold
+  21 Roll Fold (4 panels)
+  22 Accordion Fold (4 panels, with scoring)
+  23 Double Gate (with scoring)
+  24 Double Parallel (with scoring)
+  25 French Fold (with scoring)
+  26 Roll Fold (4 panels, with scoring)
+  27 Accordion Fold (4 panels, scored only)
+  28 Double Gate (scored only)
+  29 Double Parallel (scored only)
+  30 Roll Fold (4 panels, scored only)
+  --- 6-Panel ---
+  32 Half Fold then Tri-Fold
+  */
+  // await changeOneOptions(page, form, 8, 2);
 
   console.log("END");
   await browser.close();
