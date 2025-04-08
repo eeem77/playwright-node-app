0b5bcafe83b19105c1afa2b1306bd4f036638e4f
eeem77 <escobarmernestoe@gmail.com>
2025-04-08 12:56:07 -0400

    update

--- function_list.js
+++ function_list.js
@@ -1689,7 +1689,7 @@ export const getPricesProducts = async (page) => {
     let pricesTotal = 0;
     let totalSumPrice = 0;
     let pricesModel = [];
-    fs.appendFileSync("list-audit-prices-acrylic.txt", `${id},`);
+    fs.appendFileSync("list-audit-prices-laser-cut.txt", `${id},`);
     for await (const element of qtyFromInputs) {
       // const inputValue = await element.inputValue();
       const inputPrice = await prices[flag].inputValue();
@@ -1742,7 +1742,7 @@ export const getPricesProducts = async (page) => {
         }
       } catch (error) {
         fs.appendFileSync(
-          "list-audit-prices-acrylic.txt",
+          "list-audit-prices-laser-cut.txt",
           "ERROR PRODUCT OPTIONS"
         );
         // fs.appendFileSync("model-check-simple-flat.txt", "ERROR PRODUCT OPTIONS");
@@ -1751,7 +1751,7 @@ export const getPricesProducts = async (page) => {
     }
     let report = totalSumPrice + pricesTotal;
     // let report = pricesModel.toString();
-    fs.appendFileSync("list-audit-prices-acrylic.txt", `${report}\n`);
+    fs.appendFileSync("list-audit-prices-laser-cut.txt", `${report}\n`);
     // fs.appendFileSync("model-check-simple-flat.txt", `\n`);
   }
 };
@@ -1765,13 +1765,13 @@ export const getAttributes = async (page) => {
     await page.waitForSelector(".table-responsive");
     const table = await page.$(".table-responsive");
     const blocks = await table.$$('[id^="prod_add_opt_id"]')
-    fs.appendFileSync("list-audit-prices-acrylic.txt", `${id} ---> `);
+    fs.appendFileSync("list-audit-prices-laser-cut.txt", `${id} ---> `);
     for await (const block of blocks) {
       const attributes = await block.$$("span.badge.badge-info");
       const attributesLen = attributes.length;
-      fs.appendFileSync("list-audit-prices-acrylic.txt", `${attributesLen},`);
+      fs.appendFileSync("list-audit-prices-laser-cut.txt", `${attributesLen},`);
     }
-    fs.appendFileSync("list-audit-prices-acrylic.txt", `\n`);
+    fs.appendFileSync("list-audit-prices-laser-cut.txt", `\n`);
   }
 }
 
