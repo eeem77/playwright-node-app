a6578fa107705fc9adb29658e61e38985cd8b621
eeem77 <escobarmernestoe@gmail.com>
2025-05-06 14:35:56 -0400

    update

--- function_list.js
+++ function_list.js
@@ -1681,27 +1681,27 @@ export const auditProductOptions = async (page) => {
 
 export const getPricesProducts = async (page) => {
   for await (const id of idProducts) {
-    await page.goto(
-      `https://www.apprinting.com/admin/product_price.php?product_id=${id}`,
-      { timeout: 300000 }
-    );
-    const table = await page.$(".table-responsive");
-    const qtyFromInputs = await table.$$('[data-label="Quantity From"]');
-    const prices = await table.$$('[data-label="Price"]');
-    let flag = 0;
-    let pricesTotal = 0;
-    let totalSumPrice = 0;
+    // await page.goto(
+    //   `https://www.apprinting.com/admin/product_price.php?product_id=${id}`,
+    //   { timeout: 300000 }
+    // );
+    // const table = await page.$(".table-responsive");
+    // const qtyFromInputs = await table.$$('[data-label="Quantity From"]');
+    // const prices = await table.$$('[data-label="Price"]');
+    // let flag = 0;
+    // let pricesTotal = 0;
+    // let totalSumPrice = 0;
     let pricesModel = [];
-    fs.appendFileSync("list-audit-prices-is-wedding-menu.txt", `${id},`);
-    for await (const element of qtyFromInputs) {
-      // const inputValue = await element.inputValue();
-      const inputPrice = await prices[flag].inputValue();
-      flag++;
-      totalSumPrice = totalSumPrice + Number(inputPrice);
-      const report = `${totalSumPrice},`;
-      // fs.appendFileSync("list-audit-prices-foil.txt", report);
-      // console.log(report);
-    }
+    // fs.appendFileSync("list-audit-prices-is-wedding-menu.txt", `${id},`);
+    // for await (const element of qtyFromInputs) {
+    //   // const inputValue = await element.inputValue();
+    //   const inputPrice = await prices[flag].inputValue();
+    //   flag++;
+    //   totalSumPrice = totalSumPrice + Number(inputPrice);
+    //   const report = `${totalSumPrice},`;
+    //   // fs.appendFileSync("list-audit-prices-foil.txt", report);
+    //   // console.log(report);
+    // }
     await page.goto(
       `https://www.apprinting.com/admin/product_additionalinfo_list.php?product_id=${id}`,
       { timeout: 300000 }
@@ -1728,7 +1728,7 @@ export const getPricesProducts = async (page) => {
         for await (const element of prices) {
           const price = await element.inputValue();
           if (price !== "") {
-            pricesTotal = pricesTotal + Number(price);
+            // pricesTotal = pricesTotal + Number(price);
             pricesModel.push(price);
             // const report = `${pricesTotal},`;
             // const report = `${pricesModel},`;
@@ -1752,8 +1752,8 @@ export const getPricesProducts = async (page) => {
         console.log("ERROR PRODUCT OPTIONS");
       }
     }
-    let report = totalSumPrice + pricesTotal;
-    // let report = pricesModel.toString();
+    // let report = totalSumPrice + pricesTotal;
+    let report = pricesModel.toString();
     fs.appendFileSync("list-audit-prices-is-wedding-menu.txt", `${report}\n`);
     // fs.appendFileSync("model-check-simple-flat.txt", `\n`);
   }
