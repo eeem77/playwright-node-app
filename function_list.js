b32480af9262b0512e7cb975bb7aca680c73b2b3
eeem77 <escobarmernestoe@gmail.com>
2025-05-09 07:02:26 -0400

    update files

--- function_list.js
+++ function_list.js
@@ -1692,7 +1692,7 @@ export const getPricesProducts = async (page) => {
     let pricesTotal = 0;
     let totalSumPrice = 0;
     let pricesModel = [];
-    fs.appendFileSync("list-audit-prices-is-seating-charts.txt", `${id},`);
+    fs.appendFileSync("list-audit-prices-is-save-date.txt", `${id},`);
     for await (const element of qtyFromInputs) {
       // const inputValue = await element.inputValue();
       const inputPrice = await prices[flag].inputValue();
@@ -1745,7 +1745,7 @@ export const getPricesProducts = async (page) => {
         }
       } catch (error) {
         fs.appendFileSync(
-          "list-audit-prices-is-seating-charts.txt",
+          "list-audit-prices-is-save-date.txt",
           "ERROR PRODUCT OPTIONS"
         );
         // fs.appendFileSync("model-check-simple-flat.txt", "ERROR PRODUCT OPTIONS");
@@ -1754,7 +1754,7 @@ export const getPricesProducts = async (page) => {
     }
     let report = totalSumPrice + pricesTotal;
     // let report = pricesModel.toString();
-    fs.appendFileSync("list-audit-prices-is-seating-charts.txt", `${report}\n`);
+    fs.appendFileSync("list-audit-prices-is-save-date.txt", `${report}\n`);
     // fs.appendFileSync("model-check-simple-flat.txt", `\n`);
   }
 };
@@ -1768,13 +1768,13 @@ export const getAttributes = async (page) => {
     await page.waitForSelector(".table-responsive");
     const table = await page.$(".table-responsive");
     const blocks = await table.$$('[id^="prod_add_opt_id"]');
-    fs.appendFileSync("list-audit-prices-is-seating-charts.txt", `${id} ---> `);
+    fs.appendFileSync("list-audit-prices-is-save-date.txt", `${id} ---> `);
     for await (const block of blocks) {
       const attributes = await block.$$("span.badge.badge-info");
       const attributesLen = attributes.length;
-      fs.appendFileSync("list-audit-prices-is-seating-charts.txt", `${attributesLen},`);
+      fs.appendFileSync("list-audit-prices-is-save-date.txt", `${attributesLen},`);
     }
-    fs.appendFileSync("list-audit-prices-is-seating-charts.txt", `\n`);
+    fs.appendFileSync("list-audit-prices-is-save-date.txt", `\n`);
   }
 };
 
