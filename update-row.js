c802296ed55aef94f7675fcca3f56a55f5bb7808
eeem77 <escobarmernestoe@gmail.com>
2025-08-31 14:27:21 -0400

    update

--- update-row.js
+++ update-row.js
@@ -159,7 +159,7 @@ const updateRow = async () => {
   await getSizesImagesFinal(page);
   // await getUrlClientProducts(
   //   page,
-  //   "https://www.apprinting.com/en/fancy-luxury-wedding-invitations/products/"
+  //   "https://www.apprinting.com/en/vietnamese-wedding-invitations/products/"
   // );
   // await getSizesImagesArray(page);
 
